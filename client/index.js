19735d3c8054f77c1f4ee0e58cbbfc11fe47eff4
Harish Kilaru <hckilaru@gmail.com>
2016-07-05 14:43:35 -0400

    angular connected, working on nutritionix get request

--- client/index.js
+++ client/index.js
@@ -1,9 +1,27 @@
+var nutritionix = require('nutritionix')({
+    appId: '80bbaf64',
+    appKey: '9407cbb21fbb771a52f34ce630ef451b'
+}, false);
+
+
 angular.module('searchBar', [])
 
 .controller('searchController', function($scope) {
 
 	$scope.clickFunc = function() {
 		console.log('clicked!')
+		console.log($scope.input)
+		
+		nutritionix.v1_1.search.standard({
+		    phrase: $scope.input,
+		    results: '0:1'
+		}, function (err, results){
+		    console.log(results);
+		    if(err) {
+		    	console.log(err)
+			}
+		});
 	}
+	$scope.input = ''
 
 });
